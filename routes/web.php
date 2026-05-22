ea6d19d2387e1d557ba921037a96df4b6beb8369
Amruthkumarvatti <amruthkumar@example.com>
2026-05-23 00:10:36 +0530

    fixed homepage route

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,9 @@ use App\Http\Controllers\ReportController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return "Laravel app working!";
+});
 // ─── Public Routes ───────────────────────────────────────────────────────────
 Route::get('/', fn () => redirect()->route('login'));
 
